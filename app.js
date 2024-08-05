5e7bb6bea44e67dcb6c85f1e3730549d71736baa
christian <christiandeveloper123@gmail.com>
2024-08-05 12:46:25 -0300

    criação da interface inicial

---
+++ app.js
@@ -0,0 +1,16 @@
+
+class Contato {
+    constructor(nome, telefone, id) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.id = id
+    }
+   
+
+}
+
+class GerenciadorContatos {
+    constructor([]) {
+
+    }
+}
