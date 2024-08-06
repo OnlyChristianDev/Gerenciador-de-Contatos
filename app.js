51474f3cd007897ca9259c503c9faca415a05700
christian <christiandeveloper123@gmail.com>
2024-08-06 15:56:39 -0300

    atualizaçoes do js

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+import uuid from 'uuid'
+
+const noContacts = document.querySelector(".noContacts")
+const btn = document.querySelector('.btn')
+const contacts = document.querySelector(".contacts")
+const NameInput = document.querySelector (".nameInput")
+const phoneNumber = document.querySelector(".phoneNumber")
+
+const RemoveNoContact = () => {
+    noContacts.remove()
+}
+
+const newElement = () => {
+    const ul = document.createElement('ul')
+    const li = document.createElement('li')
+   
+    const contato = new Contato(inputValue, 878787, 878)
+    li.textContent = `${contato.nome}`
+    ul.appendChild(li)
+    contacts.appendChild(ul)
+}
+
+const handleEvent = () => {
+    RemoveNoContact()
+    newElement()
+}
+
+btn.addEventListener("click", handleEvent)
 
 class Contato {
     constructor(nome, telefone, id) {
@@ -5,12 +33,33 @@ class Contato {
         this.telefone = telefone;
         this.id = id
     }
-   
-
 }
 
 class GerenciadorContatos {
-    constructor([]) {
+    constructor() {
+        this.contatos = []
+        this.atualizarLista()
+    }
+
+    adicionarContato(nome, telefone){
+        const contato = new Contato(nome, telefone);
+        this.contatos.push(contato);
+    }
+
+    removerContato(id){
+        this.contatos
+    }
+    atualizarLista(){
 
     }
 }
+
+let inputValue = NameInput.value
+let inputNumberValue = phoneNumber.value 
+
+const gerenciador = new GerenciadorContatos
+gerenciador.adicionarContato(inputValue, inputNumberValue)
+console.log(this.contatos)
+
+const iniqeid = uuid.v4()
+console.log(iniqeid)
